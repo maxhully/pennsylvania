4f6ae77810b8582421b6566ce40bdf992b2805e6
Max Hully <max.hully@gmail.com>
2018-07-09 17:45:45 -0400

    First commit

---
+++ main.py
@@ -0,0 +1,46 @@
+from graphmaker.graph import Graph
+from rundmcmc.defaults import BasicChain
+from rundmcmc.partition import Partition
+from rundmcmc.updaters import (votes_updaters, Tally, perimeters, exterior_boundaries,
+                               interior_boundaries, boundary_nodes, cut_edges, polsby_popper,
+                               cut_edges_by_part)
+from rundmcmc.scores import mean_median, mean_thirdian, efficiency_gap
+from rundmcmc.output import pipe_to_table, p_value_report
+
+import functools
+
+
+def run_pa(plan='CD_CR'):
+    graph = Graph.load('./wes_graph.json').graph
+
+    assignment = {node: graph.nodes[node][plan] for node in graph.nodes}
+
+    updaters = {
+        **votes_updaters(['VoteA', 'VoteB']),
+        'population': Tally('POP100', alias='population'),
+        'perimeters': perimeters,
+        'exterior_boundaries': exterior_boundaries,
+        'interior_boundaries': interior_boundaries,
+        'boundary_nodes': boundary_nodes,
+        'cut_edges': cut_edges,
+        'areas': Tally('ALAND10', alias='areas'),
+        'polsby_popper': polsby_popper,
+        'cut_edges_by_part': cut_edges_by_part
+    }
+
+    partition = Partition(graph, assignment, updaters)
+
+    scores = {
+        'Mean-Median': functools.partial(mean_median, proportion_column_name='VoteA%'),
+        'Mean-Thirdian': functools.partial(mean_thirdian, proportion_column_name='VoteA%'),
+        'Efficiency Gap': functools.partial(efficiency_gap, col1='VoteA', col2='VoteB'),
+    }
+
+    initial_scores = {key: score(partition)
+                      for key, score in scores.items()}
+
+    chain = BasicChain(partition, total_steps=1000)
+
+    table = pipe_to_table(chain, scores)
+
+    return {key: p_value_report(key, table[key], initial_scores[key]) for key in scores}
